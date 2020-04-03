956973eedd2e2e81b22a0344352604e30e555e82
Dominik Biedebach <dominik@bdbch.com>
2020-04-04 00:05:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Windows Setup
+
+This is just some windows stuff to setup a new pc environment
+
+- `choco\install.bat` - Installs chocolately
+- `choco\auto.bat` - Automatically installs all necessary apps to computer
---
+++ choco/auto.bat
@@ -0,0 +1,20 @@
+echo "Install Development Tools"
+choco install git nvm docker-desktop -Y
+
+echo "Install Tools"
+choco install sharex obs-studio 7zip figma -Y
+
+echo "Install Browser"
+choco install brave -Y
+
+echo "Install Code Editors"
+choco install vscode -Y
+
+echo "Install Media"
+choco install spotify twitch -Y
+
+echo "Install Gaming Platforms"
+choco install steam origin -Y
+
+echo "Install Messengers"
+choco install discord telegram whatsapp slack -Y
